2b3de9340920fac9f5c53ab02be9f87867a31c12
Bergil <aurelien.grivollat@sfr.fr>
2015-01-06 16:08:43 +0100

    spring Kevin de merde

--- spring.h
+++ spring.h
@@ -1,20 +1,17 @@
 #ifndef _SPRING_H_
 #define _SPRING_H_
-
 class Spring
 {
 	public:
+		Spring() : p1(NULL), p2(NULL), l0(0.f), k(0.f) {}
+		Spring(Particle* _p1, Particle* _p2, const float _l0, const float _k) { set(_p1,_p2,_l0,_k) }
+		void set(Particle* _p1, Particle* _p2, const float _l0, const float _k);
+		void applyForceToParticles();
+	protected:
 		Particle* p1;
 		Particle* p2;
 		float l0;
-		float k;
-
-	protected:
-		Spring() : p1(NULL), p2(NULL), l0(0.f), k(0.f) {}
-		Spring(Particle* _p1, Particle* _p2, const float _l0, const float _k) { set(_p1,_p2,_l0,_k) }
-		void setParticle(Particle* _p1, Particle* _p2, const float _l0, const float _k);
-		void applyForceToParticles();
-
+		float k; //raideur
+		//float v; //viscosité
 };
-
 #endif _SPRING_H_
